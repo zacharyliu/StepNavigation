381e9b627c13fbaee09a2019d16c9da95fdee0dd
Zachary Liu <liu.zachary@gmail.com>
2013-07-25 10:40:52 -0400

    Added more code to support automatic calibration.

--- src/com/zacharyliu/stepnavigation/StepNavigationService.java
+++ src/com/zacharyliu/stepnavigation/StepNavigationService.java
@@ -17,25 +17,51 @@ import com.zacharyliu.stepnavigation.StepDetector.StepDetectorListener;
 
 public class StepNavigationService extends Service {
 
+	// Constants
 	private final String TAG = "StepNavigationService";
 	private final double STEP_LENGTH_METERS = 0.8; // http://www.wolframalpha.com/input/?i=step+length+in+meters
 	private final double EARTH_RADIUS_KILOMETERS = 6371;
-	private double mHeading = 0.0;
-	private double mBearing = 0.0;
-	private boolean isCalibrating;
-	private final int HISTORY_COUNT = 30;
-	private Queue<Double> history = new LinkedList<Double>();
-	private double historyAvg = 0.0;
+	private final int HISTORY_COUNT = 50;
+	private final double CALIBRATION_THRESHOLD = 10.0;
+
+	// Object storage
 	private List<ICustomSensor> sensors = new ArrayList<ICustomSensor>();
-	private double corrected;
-	private double[] currentLoc = {40.468184, -74.445385};
 	private final IBinder mBinder = new StepNavigationBinder();
 	private List<StepNavigationListener> listeners = new ArrayList<StepNavigationListener>();
-	
+	private GpsBearing gps;
+
+	// State variables
+	private boolean isCalibrating = true;
+	private boolean gpsReady = false;
+	private boolean headingReady = false;
+	private double mHeading = 0.0;
+	private double mBearing = 0.0;
+	private Queue<Double> history = new LinkedList<Double>();
+	private double correctionFactor = 0.0;
+	private double realHeading = 0.0;
+	private double[] currentLoc;
+
+	// Public interfaces, classes, and methods
 	public interface StepNavigationListener {
 		public void onLocationUpdate(double latitude, double longitude);
 	}
-	
+
+	public class StepNavigationBinder extends Binder {
+		public StepNavigationService getService() {
+			return StepNavigationService.this;
+		}
+	}
+
+	public void register(StepNavigationListener listener) {
+		Log.d(TAG, "Adding listener " + listener.toString());
+		listeners.add(listener);
+	}
+
+	public void unregister(StepNavigationListener listener) {
+		Log.d(TAG, "Removing listener " + listener.toString());
+		listeners.remove(listener);
+	}
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		Log.d(TAG, "Service bound");
@@ -46,7 +72,7 @@ public class StepNavigationService extends Service {
 				onDirectionUpdate();
 			}
 		}));
-		sensors.add(new GpsBearing(this, new GpsBearingListener() {
+		gps = new GpsBearing(this, new GpsBearingListener() {
 			@Override
 			public void onBearingUpdate(double bearing) {
 				mBearing = bearing;
@@ -55,57 +81,106 @@ public class StepNavigationService extends Service {
 
 			@Override
 			public void onLocationUpdate(double[] loc) {
-				if (isCalibrating)
+				gpsReady = true;
+				if (isCalibrating) {
+					currentLoc = loc;
 					callListeners(loc);
+				}
 			}
-		}));
+		});
+		sensors.add(gps);
 		sensors.add(new StepDetector(this, new StepDetectorListener() {
 			@Override
 			public void onStep() {
 				StepNavigationService.this.onStep();
 			}
 		}));
-		
+
 		for (ICustomSensor sensor : sensors) {
 			sensor.resume();
 		}
-		
+
 		return mBinder;
 	}
 
 	@Override
 	public void onDestroy() {
-		super.onDestroy();
 		for (ICustomSensor sensor : sensors) {
 			sensor.pause();
 		}
+		super.onDestroy();
 	}
-	
-	private void onDirectionUpdate() {
-		if (isCalibrating) {
-			double diff = mBearing - mHeading;
-			history.add(diff);
-			while (history.size() > HISTORY_COUNT) {
-				history.remove();
-			}
-			double sum = 0.0;
-			for (double item : history) {
-				sum += item;
-			}
-			historyAvg = sum / history.size();
-		}
 
-		corrected = mHeading + historyAvg;
-		if (corrected > 360)
-			corrected -= 360;
-		if (corrected < 0)
-			corrected += 360;
+	private boolean calibrate() {
+		Log.d(TAG, "Calibration begin");
+
+		// If history is not filled completely, exit
+		if (history.size() != HISTORY_COUNT)
+			return false;
+
+		// Take standard deviation
+		double sum = 0.0;
+		for (double item : history) {
+			sum += item;
+		}
+		double avg = sum / HISTORY_COUNT;
+		double var = 0.0;
+		for (double item : history) {
+			var += Math.pow(item - avg, 2);
+		}
+		var /= HISTORY_COUNT - 1;
+		double std = Math.sqrt(var);
+
+		Log.d(TAG, "Standard deviation: " + Double.toString(std)
+				+ " (threshold: " + Double.toString(CALIBRATION_THRESHOLD)
+				+ ")");
+
+		// If standard deviation above threshold, exit
+		if (std > CALIBRATION_THRESHOLD)
+			return false;
+
+		// Set factors and return
+		correctionFactor = avg;
+		Log.d(TAG, "Calibration succeeded");
+		return true;
+	}
+
+	// Private methods
+	private void onDirectionUpdate() {
+		realHeading = mHeading + correctionFactor;
+		if (realHeading > 360)
+			realHeading -= 360;
+		if (realHeading < 0)
+			realHeading += 360;
 	}
 
 	private void onStep() {
 		Log.d(TAG, "step");
 		
-		if (currentLoc == null || isCalibrating)
+		// TODO: Determine if recalibration is needed
+
+		// If GPS is on, add to the calibration history
+		if (gpsReady) {
+			// Add the current heading difference to the list
+			double diff = mBearing - mHeading;
+			history.add(diff);
+			while (history.size() > HISTORY_COUNT) {
+				history.remove();
+			}
+		}
+
+		// Try to calibrate
+		if (isCalibrating) {
+			if (!calibrate()) {
+				Log.d(TAG, "Calibration exited");
+				return;
+			} else {
+				isCalibrating = false;
+			}
+		}
+
+		// Exit if not ready for location calculation
+		if (!headingReady || currentLoc == null)
 			return;
 
 		// Calculate new location
@@ -113,7 +188,7 @@ public class StepNavigationService extends Service {
 		// (angles in radians)
 		double lat1 = Math.toRadians(currentLoc[0]); // starting latitude
 		double lon1 = Math.toRadians(currentLoc[1]); // starting longitude
-		double brng = Math.toRadians(corrected); // bearing
+		double brng = Math.toRadians(realHeading); // bearing
 		double d = STEP_LENGTH_METERS / 1000; // distance traveled
 		double R = EARTH_RADIUS_KILOMETERS; // radius of Earth
 		double lat2 = Math.asin(Math.sin(lat1) * Math.cos(d / R)
@@ -123,10 +198,11 @@ public class StepNavigationService extends Service {
 						Math.cos(d / R) - Math.sin(lat1) * Math.sin(lat2));
 		lat2 = Math.toDegrees(lat2);
 		lon2 = Math.toDegrees(lon2);
-		
-		Log.v(TAG, "delta lat: " + Double.toString(lat2-currentLoc[0]) + ", delta lon: " + Double.toString(lon2-currentLoc[1])); 
-		
-		double[] newLoc = {lat2, lon2};
+
+		Log.v(TAG, "delta lat: " + Double.toString(lat2 - currentLoc[0])
+				+ ", delta lon: " + Double.toString(lon2 - currentLoc[1]));
+
+		double[] newLoc = { lat2, lon2 };
 		currentLoc = newLoc;
 		callListeners(newLoc);
 	}
@@ -135,20 +211,6 @@ public class StepNavigationService extends Service {
 		for (StepNavigationListener listener : listeners) {
 			listener.onLocationUpdate(loc[0], loc[1]);
 		}
-	}
-	
-	public class StepNavigationBinder extends Binder {
-		public StepNavigationService getService() {
-			return StepNavigationService.this;
-		}
-	}
-	
-	public void register(StepNavigationListener listener) {
-		listeners.add(listener);
-	}
-	
-	public void unregister(StepNavigationListener listener) {
-		listeners.remove(listener);
 	}
 
 }
